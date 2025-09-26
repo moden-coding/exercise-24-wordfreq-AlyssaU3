9bbabb6e4795a9d0761f86b1b521084a86bcb4a4
AlyssaU3 <alys4321c@gmail.com>
2025-09-26 05:32:04 -0400

    24 done

--- src/word_frequencies.py
+++ src/word_frequencies.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python3
 
 def word_frequencies(filename):
-    pass
+    with open(filename, "r") as a:
+        word={}
+        full_list=[]
+        for line in a.readlines():
+            lines= line.split()
+            for words in lines:
+                final=words.strip("""!\"#$%&'()*,-./:;?@[]_""")
+                full_list.append(final)
 
+        for word1 in full_list:
+            if word1 in word:
+                        word[word1]+=1
+            else:
+                        word[word1]=1
+                #print(full_list)
+        return word    
+        
+        
 def main():
-    d = word_frequencies("src/alice.txt"))
+    d = word_frequencies("src/alice.txt")
+    print (d["Carroll"])
 
 
-    #if d["creating"] != 3:
+    # if d["creating"] != 3:
     #    print("Incorrect count for word 'creating'!")
-    #if d["Carroll"] != 3:
+    # if d["Carroll"] != 3:
     #    print("Incorrect count for word 'Carroll'!")
-    #if d["sleepy"] != 2:
+    # if d["sleepy"] != 2:
     #    print("Incorrect count for word 'sleepy'!")
-    #if d["Rabbit"] != 28:
+    # if d["Rabbit"] != 28:
     #    print("Incorrect count for word 'Rabbit'!")
-    #
-    #if len(d) != 2424:
+    
+    # if len(d) != 2424:
     #    print(f"Wrong number of words in the dictionary, should be 2424 but is {len(d)}")
     
 
